42486e50cc543cb06df83933ca3601a5eb927126
Julie Dickerson <jujudickerson@gmail.com>
2011-10-30 15:46:39 -0400

    another java attempt

--- myJSfile.js
+++ myJSfile.js
@@ -1,5 +1,17 @@
 
 
 function printButton() {
-	onclick=window.print();
+	var wnd = window.open('coupon.html');
+	wnd.print();
+}
+
+
+function printButton1() {
+	var wnd = window.open('coupon1.html');
+	wnd.print();
+}
+
+function printButton2() {
+	var wnd = window.open('coupon2.html');
+	wnd.print();
 }
